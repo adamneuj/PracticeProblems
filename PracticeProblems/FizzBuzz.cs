33ce8423cc5de1cecdfcab8da70020677e7bfa12
Adam Neujahr <adamneujahr@gmail.com>
2020-01-07 20:25:44 -0600

    Updated FizzBuzz

--- PracticeProblems/FizzBuzz.cs
+++ PracticeProblems/FizzBuzz.cs
@@ -16,7 +16,7 @@ namespace PracticeProblems
             string buzz = "Buzz";
             string fizz = "Fizz";
             string fizzBuzz = "FizzBuzz";
-            for(int i = 0; i < 100; i++)
+            for(int i = 0; i <= 100; i++)
             {
                 if(i % 5 == 0 && i % 3 == 0)
                 {
